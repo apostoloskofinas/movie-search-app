d0fb95130b400e841149370fec87efa5c98e398a
Apostolos Kofinas <kofinas.apostolos@gmail.com>
2024-07-21 22:08:06 +0300

    Add in env file the Api Key.Add gitignor ENV. Add try catch when fetching

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { useState } from 'react';
-import SearchBar from './components/SearchBar'
-import MovieList from './components/MovieList'
+import SearchBar from './components/SearchBar';
+import MovieList from './components/MovieList';
 import './App.css';
+
+const API_KEY = process.env.OMDB_APP_API_KEY;
 
 function App() {
   const [movies, setMovies] = useState([]);
 
   const fetchMovies = async (query) => {
-    const response = await fetch(`http://www.omdbapi.com/?s=${query}&apikey=`);
-    const data = await response.json();
-    if (data.Response === 'True') {
-      setMovies(data.Search);
-    } else {
-      setMovies([]);
+    try {
+      const response = await fetch(`http://www.omdbapi.com/?s=${query}&apikey=${API_KEY}`);
+      const data = await response.json();
+      if (data.Response === 'True') {
+        setMovies(data.Search);
+      } else {
+        setMovies([]);
+      }
+    } catch (error) {
+      console.error('Error fetching movies:', error);
     }
   };
 
