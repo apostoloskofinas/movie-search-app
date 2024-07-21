6f8c25fc1d1de844e9a75c773a5b26f8856145cf
Apostolos Kofinas <kofinas.apostolos@gmail.com>
2024-07-21 22:21:00 +0300

    .

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import SearchBar from './components/SearchBar';
 import MovieList from './components/MovieList';
 import './App.css';
 
-const API_KEY = process.env.OMDB_APP_API_KEY;
+const API_KEY = process.env.REACT_APP_OMDB_APP_API_KEY;
 
 function App() {
   const [movies, setMovies] = useState([]);
 
   const fetchMovies = async (query) => {
+    if (!query) {
+      setMovies([]);
+      return;
+    }
+  
     try {
       const response = await fetch(`http://www.omdbapi.com/?s=${query}&apikey=${API_KEY}`);
       const data = await response.json();
@@ -19,8 +24,9 @@ function App() {
       }
     } catch (error) {
       console.error('Error fetching movies:', error);
+      setMovies([]); // Clear the movie list on error
     }
-  };
+  };  
 
   return (
     <div className="App">
